513f26fe9f5d8489bb4c7b22b297e7afdd7e66de
lukew3 <lukew25073@gmail.com>
2023-08-29 14:54:23 -0400

    more req parsing

--- scrapers/lib.py
+++ scrapers/lib.py
@@ -33,17 +33,41 @@ def req_parser(req_string, current_subject):
     s3 = [item for item in s3 if item != '']
     m = {'type': 'AND', 'items': []}
     cur = {'type': 'AND', 'items': []}
-    for item in s3:
-        if item in ['AND', 'OR']:
-            cur['type'] = item
-        elif item[-1] == ';':
-            item = item[:-1]
-            cur['items'].append(item)
+    i = 0
+    while i < len(s3):
+        if s3[i] == 'AND':
+            cur['type'] = s3[i]
+        elif s3[i] == 'OR':
+            # IF "OR ABOVE" pattern found, concatenate those two groups to last. Else treat like AND
+            if s3[i+1][:5] == 'ABOVE':
+                cur['items'][-1] += ' ' + s3[i] + ' ' + s3[i+1]
+            else:
+                cur['type'] = s3[i]
+        elif s3[i][-1] == ';':
+            # Remove the last character ';' from the word
+            s3[i] = s3[i][:-1]
+            # Remove unnecessary nesting in cur
+            if len(cur['items']) == 0:
+                cur = s3[i]
+            else:
+                cur['items'].append(s3[i])
+            # Add cur to m items
             m['items'].append(cur)
+            # Set the type of m to the next word
+            if i != len(s3)-1:
+                m['type'] = s3[i+1]
+                i += 1
+            # Reset the cur
             cur = {'type': 'AND', 'items': []}
         else:
-            cur['items'].append(item)
+            # Add a requirement to cur group
+            cur['items'].append(s3[i])
+        i += 1
 
+    # Remove unnecessary nesting in cur
+    if len(cur['items']) == 1:
+        cur = cur['items'][0]
+    # Remove unnecessary nesting in m
     if len(m['items']) == 0:
         m = cur
     else:
